e60f18d89cfbb75a4b28b35265dc202e5c8e3603
Mhaque55 <mhaque21@montgomerycollege.edu>
2022-08-23 20:33:57 -0400

    Flake08

--- rps.py
+++ rps.py
@@ -3,14 +3,17 @@ score_user = 0
 score_comp = 0
 print('Welcome to Rock, Paper, and Scissors game')
 
+
 def user():
     user = input('Choose \'Rock\', \'Paper\', or \'Scissors\': ')
     return user
+
 
 def comp():
     options = ['Rock', 'Paper', 'Scissors']
     computer = random.choice(options)
     return computer
+
 
 def is_valid(user):
     if (user != 'Rock') & (user != 'Scissors') & (user != 'Paper'):
@@ -18,9 +21,11 @@ def is_valid(user):
         return 1
     return 0
 
+
 def series():
     user = input('How many series do you want to play? ')
     return int(user)
+
 
 def game():
     global score_comp
@@ -29,11 +34,11 @@ def game():
     while is_valid(user_input) != 0:
         user_input = user()
     comp_input = comp()
-    print('Computer chose ' + comp_input) 
+    print('Computer chose ' + comp_input)
     if (user_input == 'Scissors') & (comp_input == 'Rock'):
         print('Rock smash Scissors')
         print('Computer won this round')
-        score_comp += 1 
+        score_comp += 1
     elif (user_input == 'Rock') & (comp_input == 'Paper'):
         print('Paper beats Rock')
         print('Computer won this round')
@@ -42,7 +47,6 @@ def game():
         print('Scissors cut Paper')
         print('Computer won this round')
         score_comp += 1
-    
     elif (comp_input == 'Scissors') & (user_input == 'Rock'):
         print('Rock smash Scissors')
         print('You won this round')
@@ -55,7 +59,6 @@ def game():
         print('Scissors cut Paper')
         print('You won this round')
         score_user += 1
-    
     else:
         print('You chose ' + user_input + ' Computer chose ' + comp_input)
         print('It\'s a draw this round')
