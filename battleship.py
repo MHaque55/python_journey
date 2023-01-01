18e23fdd73a668b716c310ed98edc5f2cfe8ff75
Mhaque55 <mhaque21@montgomerycollege.edu>
2023-01-01 13:03:30 -0500

    func created

--- battleship.py
+++ battleship.py
@@ -1,18 +1,24 @@
-row_num = 0
-col_num = 0
+num_ships = 4
+placement = ["left", "right", "up", "down"]
 board = [[None] * 10] * 10
 for i in range(0, 10):
     for j in range(0, 10):
         board[i][j] = " -, - "
 
-for i in range(0, 10):
-    print("    " + str(i) + "   ", end='')
 
-print()
-
-for i in board:
-    print(row_num, end='')
-    row_num = row_num + 1
-    for j in i:
-        print("|" + j, end='|')
+def display(map):
+    row_num = 0
+    for i in range(0, 10):
+        print("    " + str(i) + "   ", end='')
     print()
+    for i in map:
+        print(row_num, end='')
+        row_num = row_num + 1
+        for j in i:
+            print("|" + j, end='|')
+        print()
+
+
+#def ai_turn(map):
+
+display(board)
